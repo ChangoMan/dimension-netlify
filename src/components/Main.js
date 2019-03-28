3327e7e214327f23ef9a10e51847ea09adb2dfe6
Hunter Chang <hunter@madwiremedia.com>
2019-03-27 18:46:13 -0600

    add action

--- src/components/Main.js
+++ src/components/Main.js
@@ -38,7 +38,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form name="contact" method="POST" action="/" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="bot-field" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
