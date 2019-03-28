3aea379aac80fa6fa1d762d4c366f9afca52e873
Hunter Chang <hunter@madwiremedia.com>
2019-03-27 18:52:18 -0600

    adding ajax

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,35 @@ import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+
+    const { name, email, message } = this.state;
 
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
 
@@ -38,19 +65,18 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" action="/" data-netlify="true" data-netlify-honeypot="bot-field">
-            <input type="hidden" name="bot-field" />
+          <form onSubmit={this.handleSubmit}>
             <div className="field half first">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" value={name} onChange={this.handleChange} />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="email" name="email" value={email} onChange={this.handleChange} />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" value={message} onChange={this.handleChange} rows="4" />
             </div>
             <ul className="actions">
               <li><input type="submit" value="Send Message" className="special" /></li>
